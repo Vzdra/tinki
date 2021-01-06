1397178abda30e5985bda70c9e7f0380af5ce88e
i-ina <76742075+i-ina@users.noreply.github.com>
2021-01-06 21:51:15 +0100

    fixed some issues in react

--- client_app/src/repository/register_repo.js
+++ client_app/src/repository/register_repo.js
@@ -1,25 +1,25 @@
 import axios from '../custom_axios/axios';
 
 const UserRegister = {
-    userRegister: (email ,password ,name , surname, skillsRetained, skillsToLearn) => {
+    userRegister: (email, password, name, surname, skillsRetained, skillsToLearn) => {
         return axios.post("/register/user", {
             "email": email,
             "password": password,
             "name": name,
             "surname": surname,
-            "type": 0,
             "skillsRetained": skillsRetained,
             "skillsToLearn": skillsToLearn
         });
     },
-    teamRegister:(email ,password,name) => {
+    teamRegister:(email, password, name, members) => {
         return axios.post("/register/team",{
             "email": email,
             "password": password,
-            "name": name
+            "name": name,
+            "members": members
         });
     },
-    companyRegister:(email ,password,name, country ,city, street) => {
+    companyRegister:(email, password, name, country, city, street) => {
         return axios.post("/register/company",{
             "email": email,
             "password": password,
