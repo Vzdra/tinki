0d7b1a99d34c114a0a74000dae2302cb79cb93c1
Vzdra <vladko.zdravkovski@live.com>
2021-01-15 03:52:41 +0100

    preparing edit

--- client_app/src/components/item_components/job_component.js
+++ client_app/src/components/item_components/job_component.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import {Header, List, Segment, Button} from 'semantic-ui-react';
+import {Link} from "react-router-dom";
 
 const Jobs = (props) => {
     return(
         <Segment>
             <Header as="h3">{props.title}</Header>
-            {props.type!=="USER" ? <Button primary>Edit</Button> : <span> </span>}
+            {props.type!=="USER" ? <Button primary as={Link} to={{
+                pathname: "/edit/job",
+                state: {
+                    jobId: props.id,
+                    accId: props.accId,
+                    type: props.type
+                }
+            }}>Edit</Button> : <span> </span>}
             <Segment>{props.description}</Segment>
             <Segment>
-                <Header as="h5" color="gray">{props.accountName}</Header>
+                <Header as="h5" color="grey">{props.accountName}</Header>
                 <Header as="h4" color="blue">Owner Mail: {props.accountEmail}</Header>
             </Segment>
             <Segment>
                 <Header as="h3">Skills Required:</Header>
                 <List>
                     {props.skills.map(skill => {
-                        return <List.Item>{skill.name}</List.Item>
+                        return <List.Item key={skill.id}>{skill.name}</List.Item>
                     })}
                 </List>
             </Segment>
--- client_app/src/components/item_components/project_component.js
+++ client_app/src/components/item_components/project_component.js
@@ -9,14 +9,14 @@ const Projects = (props) => {
             {props.type!=="USER" ? <Button primary>Edit</Button> : <span> </span>}
             <Segment>{props.description}</Segment>
             <Segment>
-                <Header as="h5" color="gray">{props.accountName}</Header>
+                <Header as="h5" color="grey">{props.accountName}</Header>
                 <Header as="h4" color="blue">Owner Mail: {props.accountEmail}</Header>
             </Segment>
             <Segment>
                 <Header as="h3">Skills Required:</Header>
                 <List>
                     {props.skills.map(skill => {
-                        return <List.Item>{skill.name}</List.Item>
+                        return <List.Item key={skill.id}>{skill.name}</List.Item>
                     })}
                 </List>
             </Segment>
